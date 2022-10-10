9aebc7585f10420520a2fcbc90e7a3ba6bb5eade
San-Moro <valentinacortese.dsg@gmail.com>
2022-10-10 18:43:11 +0200

    correzione funzionamento programma

--- script/script.js
+++ script/script.js
@@ -5,24 +5,26 @@
 let i ="";
 
 for (let i = 0; i <= 100; i++) {
-    console.log(i);
 
     // Se numero multiplo sia di 3 che di 5 stampi “FizzBuzz”
 
     let fizzBuzz = "";
-    let fizz = "";
-    let buzz = "";
 
     if (i % 3 === 0 && i % 5 === 0) {
       fizzBuzz = "FizzBuzz";
       console.log(fizzBuzz);
         // Se numero multiplo di 3 stampi “Fizz”
     } else if (i % 3 ===0) {
-        fizz = "Fizz";
-        console.log(fizz);
-        // Se numero multiplo di 3 stampi “Fizz”
+        fizzBuzz = "Fizz";
+        console.log(fizzBuzz);
+        // Se numero multiplo di 5 stampi “Buzz”
     } else if (i % 5 ===0) {
-        buzz = "Buzz";
-        console.log(buzz);
+        fizzBuzz = "Buzz";
+        console.log(fizzBuzz);
+    }
+    else {
+        fizzBuzz = i;
+        console.log(fizzBuzz);
     }
 }
+
